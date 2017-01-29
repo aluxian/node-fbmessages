0b5420bbe78bef085aae62874183167dc570b31d
Alexandru Rosianu <me@aluxian.com>
2017-01-29 11:35:20 +0000

    Add builders

---
+++ index.js
@@ -0,0 +1,11 @@
+const FacebookMessage = require('./lib/FacebookMessage');
+const GenericMessage = require('./lib/GenericMessage');
+const ImageMessage = require('./lib/ImageMessage');
+const TextMessage = require('./lib/TextMessage');
+
+module.exports = {
+  FacebookMessage,
+  GenericMessage,
+  ImageMessage,
+  TextMessage,
+};
